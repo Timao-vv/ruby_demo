a8776f8af54031e5567e3d2308e92e97e8725aec
Timao-vv <1456059834@qq.com>
2019-10-18 09:16:55 +0800

    two

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,24 @@
 class BooksController < ApplicationController
+
+  def show
+    @article = Article.find(params[:id])
+  end
+ 
+  def new
+  end
+ 
+  def create
+    # @book = Book.new(params[:books])
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to @book
+
+    # render plain: params[:books].inspect
+  end
+
+  private
+  def book_params
+    params.require(:books).permit(:title, :text)
+  end
+
 end
